3eea7f3bdea081b2e68b2f0ee9920d6c85c4ebf1
olhapinchuk <pinchukolya@gmail.com>
2021-03-04 16:08:29 +0100

    update(ToDoList.java): add methods WIP

--- src/todolist/ToDoList.java
+++ src/todolist/ToDoList.java
@@ -3,9 +3,21 @@ import java.util.ArrayList;
 import java.io.Serializable;
 
 public class ToDoList implements Serializable {
-    private ArrayList<Task> todos;
+    private ArrayList<Task> tasks;
 
     public ToDoList() {
-        todos = new ArrayList<>();
+        tasks = new ArrayList<>();
     }
+
+    private boolean addTask(String title, String project, String deadline) {
+       return tasks.add(new Task(title, project,deadline));
+    }
+
+    private boolean removeTask(Task task) {
+        return tasks.remove(task);
+    }
+
+//    private void editTask(Task task){
+//
+//    }
 }
