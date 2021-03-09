8b97d2814ad010f5922055b067e01ccd871d2c5c
olhapinchuk <pinchukolya@gmail.com>
2021-03-09 11:53:22 +0100

    update App and AppTest WIP

--- app/src/main/java/todolist/App.java
+++ app/src/main/java/todolist/App.java
@@ -3,12 +3,11 @@
  */
 package todolist;
 
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        InputReader reader = new InputReader();
+        reader.start();
     }
 }
