c9a0347e78842138f280a56f4c86871d6d34e3cc
olhapinchuk <pinchukolya@gmail.com>
2021-03-16 22:31:39 +0100

    update(InputReader): implement new functionality WIP

--- app/src/main/java/todolist/InputReader.java
+++ app/src/main/java/todolist/InputReader.java
@@ -6,10 +6,15 @@ import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class InputReader {
-    private Scanner reader;
-    private ToDoList tasks;
-    TaskManager [] taskManagers = { new ListPrinter(), new Creator(), new Editor(), new FileWriter() };
-
+    private final Scanner reader;
+    public static ToDoList tasks;
+    private final TaskManager [] taskManagers = { new ListPrinter(), new Creator(), new Editor(), new FileWriter() };
+    private final String [] options = {
+            "Show Task List (by date or project)",
+            "Add New Task",
+            "Edit Task (update, mark as done, remove)",
+            "Save to File and Quit"
+    };
 
     /**
      * Create a new InputReader that reads input from the text terminal.
@@ -19,21 +24,19 @@ public class InputReader {
         tasks = new ToDoList();
     }
 
-    /**
-     * Print out the welcoming message for the user.
-     */
-
     public void start() {
+        boolean stop = false;
         printWelcome();
         printTasksStatus();
 
         int choice = 0;
-        while(choice <= 0 || choice >= taskManagers.length) {
+        while(choice <= 0 || choice > taskManagers.length || !stop) {
             try {
-                printOptions();
+                Messages.printOptions(options);
                 choice = reader.nextInt();
                 System.out.println("Your choice: " + taskManagers[choice-1].getClass().getSimpleName());
-                taskManagers[choice-1].run();
+                stop = taskManagers[choice-1].run();
+
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Error: Invalid Menu Selection.");
                 System.out.println("Available options");
@@ -45,25 +48,37 @@ public class InputReader {
         }
         reader.close();
     }
+
+    /**
+     * Print out the welcoming message for the user.
+     */
+
     public void printWelcome() {
         System.out.println();
         System.out.println("Welcome to your awesome ToDo List");
     }
 
+    /**
+     * Print out the message with tasks status.
+     */
+
     private void printTasksStatus() {
-        System.out.println("You have " + tasks.getNotCompetedCount() + " todo and " + tasks.getCompletedCount() + " tasks are done!");
+        System.out.println("You have " + InputReader.tasks.getIncompetedCount() + " todo and " + InputReader.tasks.getCompletedCount() + " tasks are done!");
     }
 
-    private void printOptions() {
-        String [] options = {
-                "Show Task List (by date or project)",
-                "Add New Task",
-                "Edit Task (update, mark as done, remove)",
-                "Save to File and Quit"
-        };
 
-        IntStream.range(0, taskManagers.length)
-                .forEach(i -> System.out.println("\t (" + (i+1) + ") - " + options[i]));
+
+    /**
+     * Saves user's text input. Checks if it's any.
+     * @return String with user input.
+     */
+    public static String saveUserTextInput() {
+        Scanner keyboard = new Scanner(System.in);
+        String validInput;
+        while((validInput = keyboard.nextLine()).isEmpty()) {
+            System.out.println("Input is empty. Please try again");
+        }
+        return validInput;
     }
 
 
