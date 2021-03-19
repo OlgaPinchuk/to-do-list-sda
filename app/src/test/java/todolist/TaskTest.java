79b05086e71c488f99f41fa481f38e9f1f761e6a
olhapinchuk <pinchukolya@gmail.com>
2021-03-19 19:17:05 +0100

    update(TaskTest, ToDoListTest): add tests

--- app/src/test/java/todolist/TaskTest.java
+++ app/src/test/java/todolist/TaskTest.java
@@ -2,6 +2,62 @@ package todolist;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class TaskTest {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Test
+    void getTaskTitle() {
+        Task test = new Task("Test task 1", "Test app", LocalDate.parse("2021-03-15", formatter) );
+        assertEquals("Test task 1", test.getTitle());
+    }
+
+    @Test
+    void setTaskTitle() {
+        Task test = new Task("Test task 1", "Test app", LocalDate.parse("2021-03-15", formatter) );
+        test.setTitle("New title");
+        assertEquals("New title", test.getTitle());
+    }
+
+    @Test
+    void getTaskProjectName() {
+        Task test = new Task("Test task 1", "Test app", LocalDate.parse("2021-03-15", formatter) );
+        assertEquals("Test app", test.getProject());
+    }
+
+    @Test
+    void setTaskProjectName() {
+        Task test = new Task("Test task 1", "Test app", LocalDate.parse("2021-03-15", formatter) );
+        test.setProject("Even more test app");
+        assertEquals("Even more test app", test.getProject());
+    }
+
+    @Test
+    void getNewTaskCompletedProperty() {
+        Task test = new Task("Test task 1", "Test app", LocalDate.parse("2021-03-15", formatter) );
+        assertFalse(test.isCompleted());
+    }
+
+    @Test
+    void setTaskCompleted() {
+        Task test = new Task("Test task 1", "Test app", LocalDate.parse("2021-03-15", formatter) );
+        test.setCompleted();
+        assertTrue(test.isCompleted());
+    }
+
+    @Test
+    void getDueDate() {
+        Task test = new Task("Test task 1", "Test app", LocalDate.parse("2021-03-15", formatter) );
+        assertEquals(LocalDate.parse("2021-03-15", formatter), test.getDueDate());
+    }
+
+    @Test
+    void setDueDate() {
+        Task test = new Task("Test task 1", "Test app", LocalDate.parse("2021-03-15", formatter) );
+        test.setDueDate("2021-03-18");
+        assertEquals(LocalDate.parse("2021-03-18", formatter), test.getDueDate());
+    }
+
 }
--- app/src/test/java/todolist/ToDoListTest.java
+++ app/src/test/java/todolist/ToDoListTest.java
@@ -1,10 +1,10 @@
 package todolist;
+
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class ToDoListTest {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
@@ -12,15 +12,15 @@ public class ToDoListTest {
     @Test void addTaskIncreasesListSizeByOne() {
         ToDoList tasks = new ToDoList();
         int initialTasksNumber = tasks.getSize();
-        tasks.addTask("Task 1", "SDA",  LocalDate.parse("2021-03-10", formatter));
+        tasks.addTask("Task 1", "SDA",  LocalDate.parse("2021-03-15", formatter));
         assertEquals(initialTasksNumber+1, tasks.getSize());
     }
 
     @Test void removeTaskByIndexDecreasesListSizeByOne() {
         ToDoList tasks = new ToDoList();
-        tasks.addTask("Task 1", "SDA", LocalDate.parse("2021-03-10", formatter));
-        tasks.addTask("Task 2", "SDA", LocalDate.parse("2021-03-10", formatter));
-        tasks.addTask("Task 3", "SDA", LocalDate.parse("2021-03-10", formatter));
+        tasks.addTask("Task 1", "SDA", LocalDate.parse("2021-03-16", formatter));
+        tasks.addTask("Task 2", "SDA", LocalDate.parse("2021-03-17", formatter));
+        tasks.addTask("Task 3", "SDA", LocalDate.parse("2021-03-16", formatter));
 
         int tasksNumberBeforeRemoving = tasks.getSize();
 
@@ -31,14 +31,51 @@ public class ToDoListTest {
 
     @Test void removeTaskByTitleDecreasesListSizeByOne() {
         ToDoList tasks = new ToDoList();
-        tasks.addTask("Task 1", "SDA", LocalDate.parse("2021-03-10", formatter));
-        tasks.addTask("Task 2", "SDA", LocalDate.parse("2021-03-10", formatter));
-        tasks.addTask("Task 3", "SDA", LocalDate.parse("2021-03-10", formatter));
+        tasks.addTask("Task 1", "SDA", LocalDate.parse("2021-03-16", formatter));
+        tasks.addTask("Task 2", "SDA", LocalDate.parse("2021-03-16", formatter));
+        tasks.addTask("Task 3", "SDA", LocalDate.parse("2021-03-16", formatter));
 
         int tasksNumberBeforeRemoving = tasks.getSize();
 
         tasks.removeTaskByTitle("Task 2");
 
         assertEquals(tasksNumberBeforeRemoving - 1, tasks.getSize());
+    }
+
+    @Test void getNumberOfCompletedTasks() {
+        ToDoList tasks = new ToDoList();
+        tasks.addTask("Task 1", "SDA", LocalDate.parse("2021-03-16", formatter));
+        tasks.addTask("Task 2", "SDA", LocalDate.parse("2021-03-16", formatter));
+        tasks.addTask("Task 3", "SDA", LocalDate.parse("2021-03-16", formatter));
+
+        long tasksCompletedBefore = tasks.getCompletedCount();
+        tasks.getTask(0).setCompleted();
+        long tasksCompletedAfter = tasks.getCompletedCount();
+
+        assertEquals(tasksCompletedAfter, tasks.getCompletedCount());
+    }
+
+    @Test void sortByProject() {
+        ToDoList tasks = new ToDoList();
+        tasks.addTask("Task 1", "SDA", LocalDate.parse("2021-03-16", formatter));
+        tasks.addTask("Task 2", "Project", LocalDate.parse("2021-03-16", formatter));
+        tasks.addTask("Task 3", "SDA", LocalDate.parse("2021-03-16", formatter));
+
+       tasks.sortByProject();
+       String firstAfterSorting = tasks.getTask(0).getProject();
+
+        assertEquals("Project", tasks.getTask(0).getProject());
+    }
+
+    @Test void sortByDate() {
+        ToDoList tasks = new ToDoList();
+        tasks.addTask("Task 1", "SDA", LocalDate.parse("2021-03-19", formatter));
+        tasks.addTask("Task 2", "Project", LocalDate.parse("2021-03-16", formatter));
+        tasks.addTask("Task 3", "SDA", LocalDate.parse("2021-03-21", formatter));
+
+        tasks.sortByDate();
+        String firstAfterSorting = "Task 2";
+
+        assertEquals(firstAfterSorting, tasks.getTask(0).getTitle());
     }
 }
